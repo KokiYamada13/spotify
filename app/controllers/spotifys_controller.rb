afdc3379b93b2024476ce4cf1072cd07b67cb22b
Yamada Koki <lenny.srv.826@gmail.com>
2020-06-02 16:40:05 +0900

    Add:  アーティスト検索機能作成 - 名前、画像を表示させる

--- app/controllers/spotifys_controller.rb
+++ app/controllers/spotifys_controller.rb
@@ -1,4 +1,17 @@
 class SpotifysController < ApplicationController
+  require 'rspotify'
+  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
+
   def index
+  end
+
+  def search
+    if params[:search].present?
+      musics = RSpotify::Artist.search(params[:search])
+      @musics = musics.first
+      p @musics.name
+    end
+
+    render '_serch'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   root to: 'spotifys#index'
+
+  get '/spotifys', to: 'spotifys#search'
+  resources :spotifys do
+    collection {get "search"}
+  end
 end
